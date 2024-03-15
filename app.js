202436b2208603bc45f40d493293508a42f918b7
DuarteSVL <156538843+DuarteSVL@users.noreply.github.com>
2024-03-15 12:14:51 +0000

    Adds functions to change tables

--- app.js
+++ app.js
@@ -52,4 +52,35 @@ function makeEmentas() {
     }
 }
 
+function wipeEmentas() {
+    const tableEmenta = document.querySelector("#ementa");
+    let codigo = "";
+    tableEmenta.innerHTML = codigo;
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const btnTeste = document.getElementById("change-ementa");
+    
+        btnTeste.addEventListener("click", changeEmentas);
+});
+
+function changeEmentas(){
+    let ementa = {
+        id: "terca",
+        dia: "Terça-feira",
+        sopa: "canja",
+        prato: "frango",
+        dieta: "pescada",
+        sobremesa: "fruta",
+        lanche: "pão"
+    }
+    // ementas.splice(`${chosenElement}`, 1, ementa);
+    ementas.splice(1, 1, ementa);
+    wipeEmentas();
+    makeEmentas();
+
+}
+
+
 makeEmentas();

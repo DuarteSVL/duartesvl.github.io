62d4d6506cf4b03ea5d3c708f22714224b429511
DuarteSVL <156538843+DuarteSVL@users.noreply.github.com>
2024-03-18 17:04:26 +0000

    Navbar, tables added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     makeEmentas();
 });
 
@@ -47,7 +47,7 @@ function makeEmentas() {
                 <td>${ementa.sobremesa}</td>
                 <td>${ementa.lanche}</td>
             </tr>`;
-        
+
         tableEmenta.innerHTML += codigo;
     }
 }
@@ -59,13 +59,126 @@ function wipeEmentas() {
 }
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const btnTeste = document.getElementById("change-ementa");
-    
-        btnTeste.addEventListener("click", changeEmentas);
+
+    btnTeste.addEventListener("click", changeEmentas);
 });
 
-function changeEmentas(){
+
+// *********************************************************************************************
+// *********************************************************************************************
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("btn1");
+
+    btn.addEventListener("click", visibleA);
+    
+});
+
+function visibleA() {
+    let x = document.getElementById("ementa1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+// *********************************************************************************************
+// *********************************************************************************************
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("btn2");
+
+    btn.addEventListener("click", visibleB);
+    
+});
+
+
+
+function visibleB() {
+    let x = document.getElementById("ementa2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// *********************************************************************************************
+// *********************************************************************************************
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("btn3");
+
+    btn.addEventListener("click", visibleC);
+    
+});
+
+
+function visibleC() {
+    let x = document.getElementById("ementa3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// *********************************************************************************************
+// *********************************************************************************************
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("btn4");
+
+    btn.addEventListener("click", visibleD);
+    
+});
+
+
+function visibleD() {
+    let x = document.getElementById("ementa4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// *********************************************************************************************
+// *********************************************************************************************
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("btn5");
+
+    btn.addEventListener("click", visibleE);
+    
+});
+
+
+function visibleE() {
+    let x = document.getElementById("ementa5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+// *********************************************************************************************
+// *********************************************************************************************
+
+
+
+function changeEmentas() {
     let ementa = {
         id: "terca",
         dia: "Terça-feira",

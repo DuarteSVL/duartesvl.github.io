f85190725df12a5780fa0feffd4eedc7b884a93a
claudiopintodev <161483988+claudiopintodev@users.noreply.github.com>
2024-04-11 05:15:08 +0100

    Valencias novo temp
    Incompleto

--- app_valencia.js
+++ app_valencia.js
@@ -1,3 +1,5 @@
+/*//SWIPER
+
 // Function to ensure code execution after DOM content is fully loaded
 function domContentLoaded(callback) {
   if (document.readyState === "loading") {
@@ -104,3 +106,75 @@ domContentLoaded(function () {
     });
   });
 });
+//FIM SWIPER
+*/
+
+(function ($) {
+  ("use strict");
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  /// Navbar Fixa - torna a barra de navegação fixa quando o utilizador faz scroll na página
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $("#navbar").addClass("").css("top", "0");
+    } else {
+      $("#navbar").removeClass("shadow-sm").css("top", "-100px");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  //Header carousel ATUALIZADO
+  $(document).ready(function () {
+    $(".header-carousel").owlCarousel({
+      autoplay: false,
+      autoplayTimeout: 5000, // Autoplay interval in milliseconds
+      autoplayHoverPause: true,
+      smartSpeed: 1500, // Transition speed
+      items: 1, // Show one item at a time
+      dots: false, // Show pagination dots
+      loop: true, // Infinite loop
+      nav: true, // Show navigation arrows
+      navText: [
+        '<i class="bi bi-chevron-left"></i>',
+        '<i class="bi bi-chevron-right"></i>',
+      ],
+      responsive: {
+        0: {
+          margin: 15, // Adjust margin for smaller screens
+        },
+        768: {
+          margin: 30, // Adjust margin for medium screens
+        },
+        992: {
+          margin: 60, // Adjust margin for large screens
+        },
+      },
+    });
+  });
+
+  /*nao apagar */
+})(jQuery);

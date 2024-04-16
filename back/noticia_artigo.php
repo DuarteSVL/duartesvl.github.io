673503a83695197aea9d0f6b1637eb85db5a2ae9
DuarteSVL <156538843+DuarteSVL@users.noreply.github.com>
2024-04-16 21:51:15 +0100

    Update noticia_artigo.php

--- back/noticia_artigo.php
+++ back/noticia_artigo.php
@@ -53,32 +53,6 @@ if (isset($_GET['id'])) {
 
 
 </head>
-<!DOCTYPE html>
-<html lang="pt-pt">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detalhes da Notícia</title>
-
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
-    <!-- Custom CSS -->
-    <style>
-        /* Center the text */
-        p {
-            text-align: justify;
-        }
-
-        /* Style the container */
-        .container {
-            max-width: 800px; /* Adjust as needed */
-        }
-    </style>
-</head>
-
 <body>
     <!-- Breadcrumbs para a página -->
     <nav aria-label="breadcrumb">
@@ -127,8 +101,41 @@ if (isset($_GET['id'])) {
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
 </body>
+    </ol>
+  </nav>
 
-</html>
+  <!-- Container for content -->
+  <div class="container my-5">
+    <div class="bg-light p-4 rounded">
+      <!-- Título -->
+      <div class="d-flex justify-content-center">
+        <h3>
+          <?php echo $titulo; ?>
+        </h3>
+      </div>
+      <br><br><br>
+
+      <!-- Data -->
+      <h5>
+        <?php echo $data; ?>
+      </h5>
+
+      <div class="row">
+        <!-- Imagem -->
+        <div class="border col-md-4 float-md-end mb-3 mx-auto p-3" id="imgNoticia">
+          <img src="../img_noticia/<?php echo $imagem; ?>" class="img-fluid" alt="">
+        </div>
+
+        <!-- Texto -->
+        <div>
+          <p>
+            <?php echo $texto; ?>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
 
 
 </html>

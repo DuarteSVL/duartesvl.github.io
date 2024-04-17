e09decceffa5cac50605472ca436b35842c3ddb4
DuarteSVL <156538843+DuarteSVL@users.noreply.github.com>
2024-04-17 04:26:14 +0100

    Update make_ementas.php

--- back/make_ementas.php
+++ back/make_ementas.php
@@ -258,17 +258,22 @@
 
 
 
-    <div class="row flex justify-content-center mb-3">
-      <div class="col-lg-4 mb-2">
-        <button class="btn btn-primary btn-block" onclick="toggleTable('table1')">Ensino Básico</button>
-      </div>
-      <div class="col-lg-4 mb-2">
-        <button class="btn btn-primary btn-block" onclick="toggleTable('table2')">Creche</button>
-      </div>
-      <div class="col-lg-4 mb-2">
-        <button class="btn btn-primary btn-block" onclick="toggleTable('table3')">Berçário</button>
+    <div class="row justify-content-center mb-3">
+      <div class="col-lg-8">
+        <div class="row">
+          <div class="col-lg-4 mb-2">
+            <button class="btn btn-primary btn-block" onclick="toggleTable('table1')">Ensino Básico</button>
+          </div>
+          <div class="col-lg-4 mb-2">
+            <button class="btn btn-primary btn-block" onclick="toggleTable('table2')">Creche</button>
+          </div>
+          <div class="col-lg-4 mb-2">
+            <button class="btn btn-primary btn-block" onclick="toggleTable('table3')">Berçário</button>
+          </div>
+        </div>
       </div>
     </div>
+
 
 
 
